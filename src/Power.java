793e0fc08ba91761ee6607b50dfb357d4fb11a96
srashti333 <91120117+srashti333@users.noreply.github.com>
2022-01-27 23:55:46 +0530

    initial

---
+++ src/Power.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Power  {
+    public int num(int x,int n)
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int x = s.nextInt();
+        int n = s.nextInt();
+
+    }
+
+
+
+}
---
+++ src/Test90.java
@@ -0,0 +1,21 @@
+public class Test90 {
+    public static void ans(String inp, String ans)
+    {
+
+
+        if(inp.length() ==0 )
+        {
+            System.out.println(ans);
+            return;
+        }
+        for(int i =0;i<inp.length();i++)
+        {
+            String remainingString= inp.substring(0,i)+ inp.substring(i+1) ;
+            char extract = inp.charAt(i);
+            ans(remainingString, ans +extract);
+        }
+    }
+    public static void main(String[] args) {
+        ans("abc"," ");
+    }
+}
